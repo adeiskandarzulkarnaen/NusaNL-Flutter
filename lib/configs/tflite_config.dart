51aaa991360d9ce36a62641d9db9d84fbd70e253
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2024-08-20 06:17:24 +0700

    update model

--- lib/configs/tflite_config.dart
+++ lib/configs/tflite_config.dart
@@ -1,8 +1,8 @@
 
 
 class TfLiteConfig {
-  static const String tfliteAssetModel = "assets/models/model_satuduatiga.tflite";
-  static const String tfliteAssetLabels = "assets/models/labels_satuduatiga.txt";
+  static const String tfliteAssetModel = "assets/models/modelsunda.tflite";
+  static const String tfliteAssetLabels = "assets/models/labelsunda.txt";
   static const int tfliteInputWidth = 224;
   static const int tfliteInputHeight = 224;
 }
